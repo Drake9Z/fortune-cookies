441584060ffa58abfc855d5464a01a336aff6199
Juan Alfonso <juanchox197@gmail.com>
2023-02-26 22:27:21 -0500

    proyecto para entrega del día domingo_26

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const QuoteCard = ({changeB,phrasesData}) => {
+
+    const fondos = [ './cookie-1.jpg', './cookie-2.jpg', './cookie-3.png', './cookie-4.png']
+
+    return (
+        <div className='quote-card'>
+            <picture>
+                <img src={fondos[Math.floor(changeB/4)]} alt="background image" />
+            </picture>
+            <div>
+                <p><i className='bx bxs-quote-alt-left'></i>    {phrasesData.phrase} <i className='bx bxs-quote-alt-right' ></i></p>
+                <i className='bx bxl-react'></i>
+                <div></div>
+                <span>{phrasesData.author}</span>
+            </div>
+        </div>
+    );
+};
+
+export default QuoteCard;
